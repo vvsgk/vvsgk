5360226812933db33562c061f9e131e92170a4cd
V VENKATA SIVA GANESH KUMAR <140783484+vvsgk@users.noreply.github.com>
2025-03-07 21:50:47 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,4 +1,30 @@
-- 👋 Hi, I’m @vvsgk
-- 👀 I’m interested in nextJS
-- 🌱 I’m currently learning python and BDMS
-- 📫 How to reach me @vadlavalli.ganesh
+<h1 align="center">Hi 👋, I'm Ganesh Kumar</h1>
+<h3 align="center">Passionate BTech Student | Learning, Building, Innovating</h3>
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=vvsgk&label=Profile%20views&color=0e75b6&style=flat" alt="vvsgk" /> </p>
+
+<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=vvsgk" alt="vvsgk" /></a> </p>
+
+- 🔭 I’m currently working on [Financial Management Dashboard](https://vvsgk.github.io/fmd/)
+
+- 🌱 I’m currently learning **web development using Next.js 🚀**
+
+- 🤝 I’m looking for help with exhausted API's [Travel Planner](https://vvsgk.github.io/TravelPlanner/)
+
+- 👨‍💻 All of my projects are available at [{under_build}]({under_build})
+
+- 📫 How to reach me **vadlavalli@icloud.com**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://twitter.com/vvsgk" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="vvsgk" height="30" width="40" /></a>
+<a href="https://linkedin.com/in/vvsgk" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="vvsgk" height="30" width="40" /></a>
+<a href="https://instagram.com/ganesh.vadlavalli" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="ganesh.vadlavalli" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://aws.amazon.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" alt="aws" width="40" height="40"/> </a> <a href="https://azure.microsoft.com/en-in/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/microsoft_azure/microsoft_azure-icon.svg" alt="azure" width="40" height="40"/> </a> <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://www.java.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40"/> </a> <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/> </a> <a href="https://nestjs.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nestjs/nestjs-plain.svg" alt="nestjs" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> </p>
+
+<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=vvsgk&show_icons=true&locale=en&layout=compact" alt="vvsgk" /></p>
+
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=vvsgk&show_icons=true&locale=en" alt="vvsgk" /></p>
